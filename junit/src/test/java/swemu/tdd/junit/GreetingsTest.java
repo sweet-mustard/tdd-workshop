86c1afc920fed575f16a9b8d153627f3be804561
Stijn Van Bael <stijn.vanbael@sweetmustard.be>
2022-06-02 14:30:45 +0200

    Update style

--- junit/src/test/java/swemu/tdd/junit/GreetingsTest.java
+++ junit/src/test/java/swemu/tdd/junit/GreetingsTest.java
@@ -1,16 +1,18 @@
 package swemu.tdd.junit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GreetingsTest {
     @Test
-    void sayHelloShouldSayHello() {
+    @DisplayName("sayHello() should return 'hello'")
+    void sayHello() {
         var greetings = new Greetings();
 
         var hello = greetings.sayHello();
 
-        assertEquals(hello, "Hello");
+        assertEquals("Hello", hello);
     }
 }
